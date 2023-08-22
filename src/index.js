4436bd2c0f7415adf1e007df6ff907d44bb21715
彭格列 <1801688332@qq.com>
2023-08-22 16:56:08 +0800

    fix: update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>
 );
 
 
